d831630d8a6023b210fd9c5b4e107d0075e14fb6
blahblahradio <sounakinleuven@gmail.com>
2024-02-20 17:56:35 +0100

    Updating overview.py

--- pages/overview.py
+++ pages/overview.py
@@ -1,7 +1,10 @@
 from dash import dcc, html, dash
+from dash.dependencies import Input, Output
+from dash import callback
 import folium
 import dash
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import folium
 from folium.plugins import MarkerCluster
 import pandas as pd
@@ -18,9 +21,8 @@ aed_bxl = aed_bxl.dropna(subset=['latitude', 'longitude'])
 mug_bxl = mug_bxl.dropna(subset=['latitude', 'longitude'])
 interventions_bxl_map = interventions_bxl_map.dropna(subset=['latitude_permanence', 'longitude_permanence']).sample(2000)
 
-
 # Define different icons for each type of location
-icons = ['images/Aed_logo.jpg', 'images/mug_logo.png', 'images/Ambulance_logo.png', 'images/pit_logo.png', 'images/firetruck.png', 'images/decontamination.png']
+icons = ['assets/Aed_logo.jpg', 'assets/mug_logo.png', 'assets/Ambulance_logo.png', 'assets/pit_logo.png', 'assets/firetruck.png', 'assets/decontamination.png']
 
 # Create Folium map
 mymap = folium.Map(location=[50.8503, 4.3517], zoom_start=12) # Centered around BXL
@@ -53,12 +55,10 @@ for index, row in interventions_bxl_map[interventions_bxl_map['vector_type']=='B
     icon = folium.CustomIcon(icons[3], icon_size=(32, 32))
     folium.Marker([row['latitude_permanence'], row['longitude_permanence']], icon=icon).add_to(marker_cluster)
 
-# Add markers for Decontanimatieziekenwagen
-for index, row in interventions_bxl_map[interventions_bxl_map['vector_type']=='Decontanimatieziekenwagen'].iterrows():
+# Add markers for Decontaminatieziekenwagen
+for index, row in interventions_bxl_map[interventions_bxl_map['vector_type']=='Decontaminatieziekenwagen'].iterrows():
     icon = folium.CustomIcon(icons[3], icon_size=(32, 32))
     folium.Marker([row['latitude_permanence'], row['longitude_permanence']], icon=icon).add_to(marker_cluster)
-
-
 
 # Convert Folium map to HTML
 map_html = mymap.get_root().render()
@@ -66,5 +66,63 @@ map_html = mymap.get_root().render()
 # Define the layout of the overview page
 layout = html.Div([
     html.H1('Overview'),
-    html.Iframe(srcDoc=map_html, width='100%', height='600')
+    dbc.Row([
+        dbc.Col(
+            dcc.Dropdown(
+                id='location-type-dropdown',
+                options=[
+                    {'label': 'AED', 'value': 'AED'},
+                    {'label': 'MUG', 'value': 'MUG'},
+                    {'label': 'Ambulance', 'value': 'Ambulance'},
+                    {'label': 'PIT', 'value': 'PIT'},
+                    {'label': 'Brandziekenwagen', 'value': 'Brandziekenwagen'},
+                    {'label': 'Decontaminatieziekenwagen', 'value': 'Decontaminatieziekenwagen'}
+                ],
+                value='AED',
+                clearable=False
+            ),
+            width=3
+        ),
+        dbc.Col(
+            html.Iframe(id='map', srcDoc=map_html, width='100%', height='600'),
+            width=9
+        )
+    ])
 ])
+
+# Define callback to update the map based on dropdown selection
+@callback(
+    Output('map', 'srcDoc'),
+    [Input('location-type-dropdown', 'value')]
+)
+
+def update_map(location_type):
+    mymap = folium.Map(location=[50.8503, 4.3517], zoom_start=12) # Centered around BXL
+    marker_cluster = MarkerCluster().add_to(mymap)
+    
+    if location_type == 'AED':
+        for index, row in aed_bxl.iterrows():
+            icon = folium.CustomIcon(icons[0], icon_size=(32, 32))
+            folium.Marker([row['latitude'], row['longitude']], icon=icon).add_to(marker_cluster)
+    elif location_type == 'MUG':
+        for index, row in mug_bxl.iterrows():
+            icon = folium.CustomIcon(icons[1], icon_size=(32, 32))
+            folium.Marker([row['latitude'], row['longitude']], icon=icon).add_to(marker_cluster)
+    elif location_type == 'Ambulance':
+        for index, row in interventions_bxl_map[interventions_bxl_map['vector_type']=='Ambulance'].iterrows():
+            icon = folium.CustomIcon(icons[2], icon_size=(32, 32))
+            folium.Marker([row['latitude_permanence'], row['longitude_permanence']], icon=icon).add_to(marker_cluster)
+    elif location_type == 'PIT':
+        for index, row in interventions_bxl_map[interventions_bxl_map['vector_type']=='PIT'].iterrows():
+            icon = folium.CustomIcon(icons[3], icon_size=(32, 32))
+            folium.Marker([row['latitude_permanence'], row['longitude_permanence']], icon=icon).add_to(marker_cluster)
+    elif location_type == 'Brandziekenwagen':
+        for index, row in interventions_bxl_map[interventions_bxl_map['vector_type']=='Brandziekenwagen'].iterrows():
+            icon = folium.CustomIcon(icons[3], icon_size=(32, 32))
+            folium.Marker([row['latitude_permanence'], row['longitude_permanence']], icon=icon).add_to(marker_cluster)
+    elif location_type == 'Decontaminatieziekenwagen':
+        for index, row in interventions_bxl_map[interventions_bxl_map['vector_type']=='Decontaminatieziekenwagen'].iterrows():
+            icon = folium.CustomIcon(icons[3], icon_size=(32, 32))
+            folium.Marker([row['latitude_permanence'], row['longitude_permanence']], icon=icon).add_to(marker_cluster)
+    
+    return mymap.get_root().render()
